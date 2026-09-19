c2e4856376fb2a3ce3fcad694d73bae26c05312f
akuBaruPemula <23040700074@student.umj.ac.id>
2026-09-19 14:56:24 +0700

    ganti logo baru

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -99,7 +99,7 @@ export default function Hero({ onMovieSelect, onPlay }) {
               ref={videoRef}
               src={m.video}
               poster={m.backdrop}
-              className="w-full h-full object-contain"
+              className="w-full h-full object-contain scale-110"
               playsInline
               onTimeUpdate={handleVideoProgress}
               onEnded={goNext}
